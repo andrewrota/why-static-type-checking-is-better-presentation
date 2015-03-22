bd14e79a64baa52ae486b8f9ecf4e0fb7fe1476f
andrewrota <rota.andrew@gmail.com>
2015-03-22 19:48:28 +0000

    First commit

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Why Static Type Checking is Better',
+    eventTitle: 'EmpireJS 2015',
+    useBuilds: false, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Andrew Rota',
+    company: 'JavaScript Engineer, Wayfair',
+    twitter: '@andrewrota',
+    github: 'http://github.com/andrewrota'
+  }]
+};
+
