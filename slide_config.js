50b4a6dd1b01a347c3cd167a02b576411635a35c
Andrew Rota <anrota@wayfair.com>
2015-04-18 14:31:45 -0400

    Updates

--- slide_config.js
+++ slide_config.js
@@ -18,7 +18,7 @@ var SLIDE_CONFIG = {
   // Author information
   presenters: [{
     name: '@AndrewRota',
-    company: 'JavaScript Engineer, <img src="images/wflogo.png" style=" height: 26px; top: 3px; position: relative; margin-top: -6px;">',
+    company: 'JavaScript Engineer, Wayfair.com',
     twitter: '@andrewrota',
     github: 'http://github.com/andrewrota'
   }]
